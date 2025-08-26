3167dcc8b92140c1b70cdde8a20787513068bb3a
liuda <liuda@infrawaves.com>
2025-08-26 11:28:42 +0000

    fix NCCL_MAX_CONNS bug

--- src/include/device.h
+++ src/include/device.h
@@ -197,7 +197,7 @@ struct ncclNvls {
 #define NCCL_MAX_ARITY NCCL_MAX_DIRECT_ARITY
 #endif
 
-#define NCCL_MAX_CONNS 4
+#define NCCL_MAX_CONNS 2
 struct ncclChannelPeer {
   struct ncclConnector send[NCCL_MAX_CONNS];
   struct ncclConnector recv[NCCL_MAX_CONNS];
